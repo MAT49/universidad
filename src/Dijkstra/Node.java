425e23941bdc9e4e861e5dc1ceaf754239a647ac
MAT49 <hhwang9@student.gsu.edu>
2016-04-07 00:44:55 -0400

    Revised using native Neighbor class to describe neighboring node information.

--- src/Dijkstra/Node.java
+++ src/Dijkstra/Node.java
@@ -11,25 +11,26 @@ import java.util.ArrayList;
 class Node {
     // I'm not declaring these as private because I envision use within the package
     // also, adding getters/setters won't make it more secure at all
-    int nodeIndex;  // id for current node
-    int minDist = Integer.MAX_VALUE;   // minimum tentative distance to selected node, initially set at positive infinity-like huge value
+    int nodeNum;  // id for current node
+    int nodeWeight = Integer.MAX_VALUE;   // minimum tentative distance to selected node, initially set at positive infinity-like huge value
     
     // creates arraylist holding next node and cost
-    ArrayList<Integer> nexts = new ArrayList<Integer>();
-    ArrayList<Integer> costs = new ArrayList<Integer>();
-       
+    ArrayList<Neighbor> neighbors = new ArrayList<Neighbor>();
+    
+
     // default constructor
     Node() {    }
     
-    // constructor takes integer and assigns it to nodeIndex
-    Node(int x) { nodeIndex = x; }
+    // constructor takes integer and assigns it to nodeNum
+    Node(int x) { nodeNum = x; }
 
+    
     // method takes two integers and adds them to two arraylists
     // and returns nothing
-    void addNode(int vertex, int weight)
-    {        
-        nexts.add(vertex);
-        costs.add(weight);
+    void addNeighbor (int nextNode, int nextNodeCost)
+    {
+        Neighbor next = new Neighbor(nextNode, nextNodeCost);
+        neighbors.add(next);
     }
-
+  
 }
