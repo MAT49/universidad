037e5b22ca337fe8fb5f90c176432f299e694612
MAT49 <hhwang9@student.gsu.edu>
2016-06-15 09:42:52 -0400

    Add files via upload

---
+++ Android/safe login/RegisterRequest.java
@@ -0,0 +1,32 @@
+package me.nonnon.loginregister;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Ha on 4/18/2016.
+ */
+public class RegisterRequest extends StringRequest {
+
+    //private static final String REGISTER_REQUEST_URL = "http://10.0.2.2/android/register2.php";
+    private static final String REGISTER_REQUEST_URL = "https://nonnon.me/android/register2.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, String password, Response.Listener<String> listener) {
+
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("password", password);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
---
+++ Android/safe login/UserAreaActivity.java
@@ -0,0 +1,30 @@
+package me.nonnon.loginregister;
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+public class UserAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etSalt = (EditText) findViewById(R.id.etSalt);
+        final EditText etHash = (EditText) findViewById(R.id.etHash);
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+
+        String message = name + " welcome to your phone";
+        welcomeMessage.setText(message);
+        etUsername.setText(username);
+
+    }
+}
